c84b71d91324404d43d37ef1bf5f457f56da3845
Denis Murashev <murashev@mail.ru>
2020-02-01 21:37:42 +0300

    [jxen-measure-data] Format support is implement

---
+++ jxen-measure-data/src/main/java/com/github/jxen/measure/format/SubstanceFormat.java
@@ -0,0 +1,29 @@
+package com.github.jxen.measure.format;
+
+import com.github.jxen.measure.data.Substance;
+import java.util.ResourceBundle;
+
+/**
+ * {@code SubstanceFormat} class is used to display substance names.
+ *
+ * @author Denis Murashev
+ *
+ * @since Measure 0.6
+ */
+public class SubstanceFormat {
+
+	/**
+	 * Formats given substance.
+	 *
+	 * @param substance substance
+	 * @return localized name
+	 */
+	public String format(Substance<?> substance) {
+		String key = "substance." + substance.getName();
+		return getBundle(substance).getString(key);
+	}
+
+	private ResourceBundle getBundle(Substance<?> substance) {
+		return ResourceBundle.getBundle(substance.getClass().getName());
+	}
+}
---
+++ jxen-measure-data/src/test/java/com/github/jxen/measure/format/SubstanceFormatTest.java
@@ -0,0 +1,16 @@
+package com.github.jxen.measure.format;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.github.jxen.measure.data.Metal;
+import java.util.Locale;
+import org.junit.jupiter.api.Test;
+
+class SubstanceFormatTest {
+
+	@Test
+	void testFormat() {
+		Locale.setDefault(Locale.US);
+		assertEquals("gold", new SubstanceFormat().format(Metal.GOLD));
+	}
+}
